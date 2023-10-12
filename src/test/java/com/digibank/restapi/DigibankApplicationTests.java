3bd8180e71e81976cba01e7e460c9b855d5e0009
baharudindayat <baharudindayat57@gmail.com>
2023-10-12 14:34:37 +0700

    Initial commit

---
+++ src/test/java/com/digibank/restapi/DigibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digibank.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
