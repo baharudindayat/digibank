ad96394700e1c1fc2e76915d093cf229d47e2e7a
alamsyhhh <alamsyah.rizki157@gmail.com>
2023-11-01 15:11:44 +0700

    update otp auth

--- src/main/java/com/digibank/restapi/digibank/service/UserService.java
+++ src/main/java/com/digibank/restapi/digibank/service/UserService.java
@@ -1,16 +1,17 @@
 package com.digibank.restapi.digibank.service;
 
+import com.digibank.restapi.digibank.config.BCrypt;
 import com.digibank.restapi.digibank.dto.RegisterDto;
 import com.digibank.restapi.digibank.entity.User;
 import com.digibank.restapi.digibank.repository.UserRepository;
 import com.digibank.restapi.digibank.util.EmailUtil;
 import com.digibank.restapi.digibank.util.OtpUtil;
 import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
@@ -77,8 +78,7 @@ public class UserService {
 
         // Implement password validation and hashing logic as needed
 
-
-        user.setPassword(password);
+        user.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
         userRepository.save(user);
 
         return "Password changed successfully";
