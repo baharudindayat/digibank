5105119fc446429680a54b13885771cbd9ac7e8f
baharudindayat <baharudindayat57@gmail.com>
2023-12-05 11:13:48 +0700

    fix rekening asal to rekening tujuan

--- src/main/java/com/digibank/restapi/service/impl/TransferServiceImpl.java
+++ src/main/java/com/digibank/restapi/service/impl/TransferServiceImpl.java
@@ -130,8 +130,8 @@ public class TransferServiceImpl implements TransferService {
         transaksiRepository.save(transaksi);
 
         Transaksi transaksiTujuan = new Transaksi();
-        transaksiTujuan.setRekeningTujuan(rekeningTujuan.get());
-        transaksiTujuan.setRekeningAsal(rekeningAsal.get());
+        transaksiTujuan.setRekeningTujuan(rekeningAsal.get());
+        transaksiTujuan.setRekeningAsal(rekeningTujuan.get());
         transaksiTujuan.setJenisTransaksi(JenisTransaksi.PINDAHBUKU);
         transaksiTujuan.setNominal(transferDto.getNominal());
         transaksiTujuan.setCatatan(transferDto.getCatatan());
@@ -190,6 +190,9 @@ public class TransferServiceImpl implements TransferService {
 
     @NotNull
     private Object getObject(Optional<Rekening> getAccountTujuan, RekeningNameDto rekeningNameDto) {
+        if (getAccountTujuan.isEmpty()) {
+            throw new TransferFailedException("Maaf! Nomor Rekening yang dituju terblokir");
+        }
         AccountStatus getUser = getAccountTujuan.get().getIdCif().getIdUsers().getStatusUser();
         if (getUser.equals(AccountStatus.TERBLOKIR)) {
             throw new TransferFailedException("Maaf! Nomor Rekening yang dituju terblokir");
